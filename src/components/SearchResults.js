6ff4a9010962f9215d3f017ed0012944a290cd10
Fedor Khodkov <fkhodkov@gmail.com>
2019-02-17 21:23:22 +0300

    Add keys

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,10 +7,10 @@ export default function SearchResults(props) {
             {props.message ? <p className="search-result-message">{props.message}</p> : null}
             {props.found ?
               Object.keys(props.found).map(key => (
-                <div className="search-result-section">
+                <div className="search-result-section" key={key}>
                     <h2>{key}</h2>
-                    {props.found[key].map(record => (
-                      <p className="search-result-item">
+                    {props.found[key].map((record, index) => (
+                      <p className="search-result-item" key={index}>
                           <span>{record.timestamp}</span>
                           <span>{record.level}</span>
                           <span>{record.message}</span>
