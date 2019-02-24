5347e7d88dcd5ab6d97ca7e95687012105821766
Andrey Ponorenko <ponorenko@yandex.ru>
2019-02-25 02:08:54 +0300

    react hw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,82 @@
-import React, { Fragment } from "react";
+import React from "react";
 
-export default function App() {
+function LogBatch(props) {
+  let logList = props.logBatch;
   return (
-      <Fragment>
-          ui toolkit : <a href="https://material-ui.com/getting-started/installation/">material-ui</a>
-      </Fragment>
+    <ul>
+      {Object.values(logList).map(log => (
+        <li>{Object.values(log).join('; ')}</li>
+      ))}
+    </ul>
   );
-};
+}
+
+function LogBatches(props) {
+  let logs = props.logs;
+  if (logs === undefined) {
+    return null;
+  } else {
+    let listOfHeaders = Object.keys(logs);
+    return (
+      <div>
+        {listOfHeaders.map(logHead =>
+          (
+            <div>
+              <h3>{logHead}</h3>
+              <LogBatch logBatch={logs[logHead]}/>
+            </div>
+          )
+        )}
+      </div>
+    );
+  }
+}
+
+export default class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {searchText: ''};
+      this.textChangeHandler = this.textChangeHandler.bind(this);
+      this.feelLuckyHandler = this.feelLuckyHandler.bind(this);
+      this.searchHandler = this.searchHandler.bind(this);
+  }
+
+  textChangeHandler(e) {
+    this.setState({searchText: e.target.value});
+  }
+
+  searchHandler(e) {
+    fetch('/api/logs?rid='+this.state.searchText)
+      .then(response => {
+        if (response.status != 200) {
+          this.setState({logs: {'Nothing to show...': []}});
+          throw new Error("Fetch failed. Status: " + response.status);
+        }
+        return response.json()
+      })
+      .then(json => this.setState({logs:json}));
+  }
+
+  feelLuckyHandler(e) {
+    fetch('/api/feelinglucky/')
+      .then(response => {
+        if (response.status != 200) {
+          throw new Error("Fetch failed. Status: " + response.status);
+        }
+        return response.json()
+      })
+      .then(json => this.setState({searchText:json.rid }));
+  }
+
+  render() {
+    return (
+        <div>
+          <input type="text" onChange={this.textChangeHandler} value={this.state.searchText}/>
+          <button onClick={this.searchHandler}>search</button>
+          <button onClick={this.feelLuckyHandler}>i'm feeling lucky</button>
+          <LogBatches logs={this.state.logs}/>
+        </div>
+    );
+  }
+}
